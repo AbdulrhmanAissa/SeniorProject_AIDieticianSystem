8547aa48684c2547f9924b6d04de0d39657bb6ec
AbdulrhmanAissa@hotmail.com <AbdulrhmanAissa@hotmail.com>
2023-09-21 16:15:53 +0300

    Changed file name directory

--- AI_DieticianSystem/assets/css/style.css
+++ AI_DieticianSystem/assets/css/style.css
@@ -1,5 +1,5 @@
 /**
-* Template Name: NiceAdmin
+* Template Name: AI_DieticianSystem
 * Updated: Sep 18 2023 with Bootstrap v5.3.2
 * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
 * Author: BootstrapMade.com
